e78e78bddef277e422926e7e852d7f2189db3068
JijiKusahara <kayokawase0519@gmail.com>
2020-08-23 00:47:16 +0900

    songs and discs fix

--- db/migrate/20200821160923_create_discs.rb
+++ db/migrate/20200821160923_create_discs.rb
@@ -1,7 +1,7 @@
 class CreateDiscs < ActiveRecord::Migration[6.0]
   def change
     create_table :discs do |t|
-      t.string :disc,               null: false
+      t.string :name,               null: false
 
       t.timestamps
     end
