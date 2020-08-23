ff2c262ae2e4ca76f089bc68f867521106ff006b
JijiKusahara <kayokawase0519@gmail.com>
2020-08-24 01:51:41 +0900

    destroy reation
    disc&song

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,5 +1,5 @@
 class Disc < ApplicationRecord
-  has_many :song_discs
+  has_many :song_discs, dependent: :destroy
   has_many :songs, through: :song_discs
   validates :name, presence: true
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
   has_many :songcolors, dependent: :destroy
 
-  has_many :song_discs
+  has_many :song_discs, dependent: :destroy
   has_many :discs, through: :song_discs
   validates :name, presence: true
 end
